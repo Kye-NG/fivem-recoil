5ad4894943cee834746737e2a888f795c76db230
Kye-NG <kyenormangill98@gmail.com>
2021-04-02 23:35:56 +1100

    [REC] Optimise recoil to only call get pedid when needed.

--- client.lua
+++ client.lua
@@ -1,20 +1,25 @@
+local currentPlayerPedId = ''
+
+AddEventHandler('playerSpawned', function()
+	currentPlayerPedId = PlayerPedId()
+end)
+
+
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 
-		-- Disable shooting from vehicles.
-		local playerPed = GetPlayerPed(-1)
-		local car = GetVehiclePedIsIn(playerPed, false)
+		local car = GetVehiclePedIsIn(currentPlayerPedId, false)
 		if car then
-			if GetPedInVehicleSeat(car, -1) == playerPed then
-				SetPlayerCanDoDriveBy(PlayerId(), false)
+			if GetPedInVehicleSeat(car, -1) == currentPlayerPedId then
+				SetPlayerCanDoDriveBy(currentPlayerPedId, false)
 			else
-				SetPlayerCanDoDriveBy(PlayerId(), false)
+				SetPlayerCanDoDriveBy(currentPlayerPedId, false)
 			end
 		end
 
 		-- Shooting check and adding recoil.
-		if IsPedShooting(PlayerPedId()) and NoRecoilCheck(playerPed) then
+		if IsPedShooting(currentPlayerPedId) and NoRecoilCheck() then
 			if GetFollowPedCamViewMode() == 4 then -- First Person
 				RecoilFirstPersonMultiplier(0.5, 0.5)
 			elseif GetFollowPedCamViewMode() ~= 4 then -- Third Person
@@ -65,16 +70,16 @@ function RecoilThirdPersonMultiplier(ThirdPersonMultiplier,ThirdPersonAimingMult
 	end
 end
 
-function NoRecoilCheck(playerPed)
-	if GetWeapontypeGroup(GetSelectedPedWeapon(playerPed)) == 2685387236 then
+function NoRecoilCheck()
+	if GetWeapontypeGroup(GetSelectedPedWeapon(currentPlayerPedId)) == 2685387236 then
 		return false
-	elseif GetWeapontypeGroup(GetSelectedPedWeapon(playerPed)) == 1548507267 then
+	elseif GetWeapontypeGroup(GetSelectedPedWeapon(currentPlayerPedId)) == 1548507267 then
 		return false
-	elseif GetWeapontypeGroup(GetSelectedPedWeapon(playerPed)) == 4257178988 then
+	elseif GetWeapontypeGroup(GetSelectedPedWeapon(currentPlayerPedId)) == 4257178988 then
 		return false
-	elseif GetSelectedPedWeapon(PlayerPedId()) == GetHashKey("weapon_minigun") then
+	elseif GetSelectedPedWeapon(currentPlayerPedId) == GetHashKey("weapon_minigun") then
 		return false
-	elseif GetSelectedPedWeapon(PlayerPedId()) == GetHashKey("weapon_rayminigun") then
+	elseif GetSelectedPedWeapon(currentPlayerPedId) == GetHashKey("weapon_rayminigun") then
 		return false
 	else
 		return true
